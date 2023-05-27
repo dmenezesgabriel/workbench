43a8c9b80ba057a8f66fc9ec8d42009e3919bb03
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-05-27 00:19:50 -0300

    Split desktop in html css and javascript files

--- desktop/script.js
+++ desktop/script.js
@@ -1,54 +1,140 @@
-// Make windows draggable
-var windows = document.getElementsByClassName("window");
-var isDragging = false;
-var dragWindow = null;
+var taskbar = document.getElementById("taskbar");
+var taskbarButtons = [];
 
-for (var i = 0; i < windows.length; i++) {
-  windows[i].addEventListener("mousedown", function (e) {
-    isDragging = true;
-    dragWindow = this;
-    this.style.zIndex = 2;
-  });
+var icons = document.getElementsByClassName("desktop-icon");
+for (var i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("click", createWindow);
+}
 
-  windows[i].addEventListener("mouseup", function (e) {
-    isDragging = false;
-  });
+function createWindow() {
+  var iconName = this.id;
+  var windowId = "window-" + Date.now();
 
-  windows[i].addEventListener("mousemove", function (e) {
-    if (isDragging && dragWindow !== null) {
-      var x = e.clientX - dragWindow.offsetWidth / 2;
-      var y = e.clientY - dragWindow.offsetHeight / 2;
-      dragWindow.style.left = x + "px";
-      dragWindow.style.top = y + "px";
+  var window = document.createElement("div");
+  window.className = "window";
+  window.id = windowId;
+
+  var titleBar = document.createElement("div");
+  titleBar.className = "title-bar";
+  titleBar.innerText = iconName;
+  var buttons = document.createElement("div");
+  buttons.className = "buttons";
+  var minimizeButton = createWindowButton("minimize");
+  var maximizeButton = createWindowButton("maximize");
+  var closeButton = createWindowButton("close");
+  buttons.appendChild(minimizeButton);
+  buttons.appendChild(maximizeButton);
+  buttons.appendChild(closeButton);
+  titleBar.appendChild(buttons);
+  window.appendChild(titleBar);
+
+  var content = document.createElement("div");
+  content.className = "content";
+  // Window content goes here...
+  window.appendChild(content);
+
+  document.body.appendChild(window);
+
+  dragElement(window);
+
+  minimizeButton.addEventListener("click", minimizeWindow);
+  maximizeButton.addEventListener("click", maximizeWindow);
+  closeButton.addEventListener("click", closeWindow);
+
+  function createWindowButton(type) {
+    var button = document.createElement("button");
+    button.className = type;
+    return button;
+  }
+}
+
+// Make the windows draggable
+function dragElement(element) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  var titleBar = element.getElementsByClassName("title-bar")[0];
+
+  if (titleBar) {
+    titleBar.onmousedown = dragMouseDown;
+  } else {
+    element.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    element.style.top = element.offsetTop - pos2 + "px";
+    element.style.left = element.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function minimizeWindow() {
+  var window = this.parentNode.parentNode.parentNode;
+  window.style.display = "none";
+
+  var windowId = window.id;
+  var taskbarButton = createTaskbarButton(windowId);
+  taskbar.appendChild(taskbarButton);
+  taskbarButtons.push(taskbarButton);
+}
+
+function maximizeWindow() {
+  var window = this.parentNode.parentNode.parentNode;
+
+  if (window.style.width === "100%" && window.style.height === "100%") {
+    window.style.width = "300px";
+    window.style.height = "200px";
+    window.style.top = "100px";
+    window.style.left = "100px";
+  } else {
+    window.style.width = "100%";
+    window.style.height = "100%";
+    window.style.top = "0";
+    window.style.left = "0";
+  }
+}
+
+function closeWindow() {
+  var window = this.parentNode.parentNode.parentNode;
+  window.parentNode.removeChild(window);
+}
+
+function createTaskbarButton(windowId) {
+  var button = document.createElement("div");
+  button.className = "taskbar-button";
+  button.innerText = windowId;
+  button.addEventListener("click", restoreWindow.bind(null, windowId));
+  return button;
+}
+
+function restoreWindow(windowId) {
+  var window = document.getElementById(windowId);
+  window.style.display = "block";
+  taskbarButtons = taskbarButtons.filter(function (button) {
+    if (button.innerText === windowId) {
+      button.parentNode.removeChild(button);
+      return false;
     }
-  });
-}
-
-// Handle minimize, maximize, and close buttons
-var minimizeButtons = document.getElementsByClassName("minimize-button");
-var maximizeButtons = document.getElementsByClassName("maximize-button");
-var closeButtons = document.getElementsByClassName("close-button");
-
-for (var i = 0; i < minimizeButtons.length; i++) {
-  minimizeButtons[i].addEventListener("click", function (e) {
-    this.parentNode.parentNode.parentNode.style.display = "none";
-  });
-}
-
-for (var i = 0; i < maximizeButtons.length; i++) {
-  maximizeButtons[i].addEventListener("click", function (e) {
-    if (this.parentNode.parentNode.parentNode.style.height === "100%") {
-      this.parentNode.parentNode.parentNode.style.height = "300px";
-      this.parentNode.parentNode.parentNode.style.width = "400px";
-    } else {
-      this.parentNode.parentNode.parentNode.style.height = "100%";
-      this.parentNode.parentNode.parentNode.style.width = "100%";
-    }
-  });
-}
-
-for (var i = 0; i < closeButtons.length; i++) {
-  closeButtons[i].addEventListener("click", function (e) {
-    this.parentNode.parentNode.parentNode.style.display = "none";
+    return true;
   });
 }
