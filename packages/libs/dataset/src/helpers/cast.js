f373b8ee2c2d3141b44a6ea118845323be166366
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-08-19 11:34:07 -0300

    Refactor cast helper

--- packages/libs/dataset/src/helpers/cast.js
+++ packages/libs/dataset/src/helpers/cast.js
@@ -1,46 +1,79 @@
-class Cast {
-  constructor(data) {
-    this._data = data;
-  }
-
-  asType(convertObject) {
-    const convertedData = [];
-    const convertMap = {};
-
-    for (const [key, targetType] of Object.entries(convertObject)) {
-      if (targetType === "string") {
-        convertMap[key] = String;
-      } else if (targetType === "number") {
-        convertMap[key] = Number;
-      } else if (targetType === "boolean") {
-        convertMap[key] = Boolean;
-      } else if (targetType.type === "Date") {
-        const options = targetType.options || {};
-        convertMap[key] = (value) => {
-          const formattedDate = new Date(value);
-          if (options.format) {
-            return formattedDate.toLocaleString(undefined, options.format);
-          } else {
-            return formattedDate;
-          }
-        };
-      }
-    }
-
-    for (const row of this._data) {
-      const convertedRow = {};
-      for (const [key, value] of Object.entries(row)) {
-        if (convertMap[key]) {
-          convertedRow[key] = convertMap[key](value);
-        } else {
-          convertedRow[key] = value;
-        }
-      }
-      convertedData.push(convertedRow);
-    }
-
-    return convertedData;
+class Converter {
+  convert(value) {
+    throw new Error("Not implemented");
   }
 }
 
+class StringConverter extends Converter {
+  convert(value) {
+    return String(value);
+  }
+}
+
+class NumberConverter extends Converter {
+  convert(value) {
+    return Number(value);
+  }
+}
+
+class BooleanConverter extends Converter {
+  convert(value) {
+    return Boolean(value);
+  }
+}
+
+class DateConverter extends Converter {
+  constructor(options) {
+    super();
+    this.options = options;
+  }
+
+  convert(value) {
+    const formattedDate = new Date(value);
+    if (this.options.format) {
+      return formattedDate.toLocaleString(undefined, this.options.format);
+    } else {
+      return formattedDate;
+    }
+  }
+}
+
+class Cast {
+  constructor(data) {
+    this._data = data;
+    this._converters = {
+      string: new StringConverter(),
+      number: new NumberConverter(),
+      boolean: new BooleanConverter(),
+      Date: new DateConverter(),
+    };
+  }
+
+  _getTargeType(target) {
+    return typeof target === "object" ? target.type : target;
+  }
+
+  _convertValue(value, target) {
+    const converters = new Map([
+      ["string", () => new StringConverter().convert(value)],
+      ["number", () => new NumberConverter().convert(value)],
+      ["boolean", () => new BooleanConverter().convert(value)],
+      ["Date", () => new DateConverter(target?.options).convert(value)],
+    ]);
+
+    const converter =
+      converters.get(this._getTargeType(target)) || converters.get("default");
+    return converter();
+  }
+
+  asType(convertObject) {
+    return this._data.map((row) => {
+      const convertedRow = {};
+      for (const [key, targetType] of Object.entries(convertObject)) {
+        convertedRow[key] = this._convertValue(row[key], targetType);
+      }
+      return convertedRow;
+    });
+  }
+}
 export { Cast };
