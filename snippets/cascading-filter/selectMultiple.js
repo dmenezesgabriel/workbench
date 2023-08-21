d5927e60efc3199d158189d72e61e0fedf3126df
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-08-20 23:08:53 -0300

    Add tags

--- snippets/cascading-filter/selectMultiple.js
+++ snippets/cascading-filter/selectMultiple.js
@@ -1,7 +1,40 @@
+class Tag {
+  constructor(name) {
+    this.name = name;
+    this.element = this.createElement();
+    this.dispose.bind(this);
+  }
+
+  dispose() {
+    this.element.dispatchEvent(
+      new CustomEvent("dispose", { detail: { name: this.name } })
+    );
+    this.element.remove();
+  }
+
+  createElement() {
+    this.element = document.createElement("div");
+    this.element.textContent = this.name;
+    this.element.appendChild(this.createDisposeButton());
+    return this.element;
+  }
+
+  createDisposeButton() {
+    const disposeButton = document.createElement("button");
+    disposeButton.textContent = "x";
+    disposeButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.dispose();
+    });
+    return disposeButton;
+  }
+}
+
 class Select {
   constructor(selectElement) {
     this.selectElement = selectElement;
     this.options = [];
+    this.tags = [];
     this.updateSelectStyles();
     this.createSearchInput();
     this.createDropdown();
@@ -10,6 +43,7 @@ class Select {
     this.observeSelectElement();
     this.addOption.bind(this);
     this.getOptions.bind(this);
+    this.addPlaceholder.bind(this);
   }
 
   addOption(optionElement) {
@@ -18,6 +52,10 @@ class Select {
 
   getOptions() {
     return this.options;
+  }
+
+  setOptionDisabled(optionElement, disabled) {
+    optionElement.disabled = disabled;
   }
 
   updateSelectStyles() {
@@ -32,12 +70,12 @@ class Select {
     this.searchInput.style.border = "1px solid #ccc";
     this.searchInput.contentEditable = true;
     this.searchInput.classList.add("search-input");
-    this.searchInput.textContent = "Search...";
     this.searchContainer.appendChild(this.searchInput);
     this.selectElement.parentNode.insertBefore(
       this.searchContainer,
       this.selectElement
     );
+    this.addPlaceholder();
   }
 
   createDropdown() {
@@ -57,6 +95,59 @@ class Select {
     this.toggle.innerHTML = "&#9660;";
     this.toggle.style.marginLeft = "5px";
     this.searchContainer.appendChild(this.toggle);
+  }
+
+  addPlaceholder() {
+    this.searchInput.textContent = "Search...";
+  }
+
+  addTag(name) {
+    if (this.isTagUnique(name)) {
+      const tag = this.createTag(name);
+      this.addTagToTags(tag);
+      this.addDisposeEventListener(tag);
+      this.renderTags();
+      this.enableOption(name);
+    }
+  }
+
+  isTagUnique(name) {
+    return this.tags.filter((tag) => tag.name === name).length === 0;
+  }
+
+  createTag(name) {
+    return new Tag(name);
+  }
+
+  addTagToTags(tag) {
+    this.tags.push(tag);
+  }
+
+  addDisposeEventListener(tag) {
+    tag.element.addEventListener("dispose", (event) => {
+      this.removeTag(event.detail.name);
+      this.renderTags();
+      this.enableOption(event.detail.name);
+    });
+  }
+
+  enableOption(name) {
+    this.getOptions().forEach((option) => {
+      if (option.textContent.toLowerCase() === name.toLowerCase()) {
+        this.setOptionDisabled(option, false);
+      }
+    });
+  }
+
+  removeTag(name) {
+    this.tags = this.tags.filter((tag) => tag.name !== name);
+  }
+
+  renderTags() {
+    this.searchInput.textContent = "";
+    for (const tag of this.tags) {
+      this.searchInput.appendChild(tag.element);
+    }
   }
 
   addEventListeners() {
@@ -86,12 +177,13 @@ class Select {
     });
 
     this.searchInput.addEventListener("focusin", (event) => {
-      this.searchInput.textContent = "";
+      if (this.searchInput.textContent === "Search...")
+        this.searchInput.textContent = "";
     });
 
     this.searchInput.addEventListener("focusout", (event) => {
       if (this.searchInput.textContent === "") {
-        this.searchInput.textContent = "Search...";
+        this.addPlaceholder();
       }
     });
 
@@ -131,6 +223,18 @@ class Select {
   handleOptionClick(option, event) {
     event.preventDefault();
     option.selected = !option.selected;
+
+    if (option.selected) {
+      this.addTag(option.textContent);
+    } else {
+      this.removeTag(option.textContent);
+    }
+
+    if (this.tags.length === 0) {
+      this.addPlaceholder();
+    } else {
+      this.renderTags();
+    }
     this.selectElement.dispatchEvent(new Event("change"));
   }
 }
